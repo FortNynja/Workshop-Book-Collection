542b9b23e2437a3622b9a9e35fcd6dc3a1525d02
Malcolm Jeremiah Rajeli <maljer@fortnynja.com>
2021-11-03 09:31:23 +0800

    Publishers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_004453) do
+ActiveRecord::Schema.define(version: 2021_11_03_013011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
