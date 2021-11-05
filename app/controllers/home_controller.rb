93582c265a4d9f2908883f9f3a264c7477e85a32
Malcolm Jeremiah Rajeli <maljer@fortnynja.com>
2021-11-05 10:19:06 +0800

    Root path

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  # Index
+  # Show
+  # New
+  # Create
+  # Edit
+  # Update
+  # Destroy
+  def index; end # => views/{ControllerName}/{Method}
+end
